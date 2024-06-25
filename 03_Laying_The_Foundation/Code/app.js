434822a9faad791adae418545fe2ea578ecd009b
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-25 23:47:41 +0530

    add some jsx notes

--- 03_Laying_The_Foundation/Code/app.js
+++ 03_Laying_The_Foundation/Code/app.js
@@ -1,19 +1,25 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 //* ReactElement(Object) => it is creating an object while it is rendering onto the dom it converts itself into that HTML(Browser understands) and put it up into the dom
 
-const heading = React.createElement("h1", {id: "heading"}, "Radha Krishna")
-console.log(heading)
+const heading = React.createElement("h1", { id: "heading" }, "Radha Krishna");
+console.log(heading);
 
 //* JSX => is not HTML in JS (HTML-like or XML-like syntax)
 //* JSX => transpiled before it reaches the JS - Parcel - Babel
-//* JSX => React.createElement => ReactElement-JS(Object) => HTMLElement(render)
+//* JSX (transpiled by Babel) => React.createElement => ReactElement => JS Object => HTML Element(render)
 
-const jsxHeading = <h1 id="heading">Hare Krishna Hare Rama</h1>
-console.log(jsxHeading)
+const jsxHeading = (
+  <h1 className="head" tabIndex="1">
+    Hare Krishna Hare Rama
+  </h1>
+);
+// const jsxHeading = (<h1 className="head" tabIndex="1">
+//     Hare Krishna Hare Rama
+//     </h1>)
+console.log(jsxHeading);
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading)
+root.render(jsxHeading);
