6d74dde0bfc1ca744d9963154468c2fba6ce6b6f
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-26 18:50:01 +0530

    React Component

--- 03_Laying_The_Foundation/Code/app.js
+++ 03_Laying_The_Foundation/Code/app.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 
 //* ReactElement(Object) => it is creating an object while it is rendering onto the dom it converts itself into that HTML(Browser understands) and put it up into the dom
 
-const heading = React.createElement("h1", { id: "heading" }, "Radha Krishna");
-console.log(heading);
+// const heading = React.createElement("h1", { id: "heading" }, "Radha Krishna");
+// console.log(heading);
 
 //* JSX => is not HTML in JS (HTML-like or XML-like syntax)
 //* JSX => transpiled before it reaches the JS - Parcel - Babel
@@ -18,8 +18,36 @@ const jsxHeading = (
 // const jsxHeading = (<h1 className="head" tabIndex="1">
 //     Hare Krishna Hare Rama
 //     </h1>)
-console.log(jsxHeading);
+// console.log(jsxHeading);
+
+//* React Functional Component
+
+const HeadingComponent1 = () => (
+  <h1 className="head">React Functional Component1</h1>
+);
+
+const HeadingComponent2 = () => {
+  return <h1 className="head">React Functional Component2</h1>;
+};
+
+const Title1 = () => <h1 tabIndex="1">Hare Krishna Hare Rama</h1>;
+
+const title = <h1 tabIndex="1">Hare Krishna Hare Rama</h1>;
+
+const number = 7;
+
+//* Component Compositions
+const HeadingComponent3 = () => (
+  <div id="container">
+    <Title1 />
+    <h1 className="head">React Functional Component3</h1>
+    <h2>{number}</h2>
+    <h2>{3 + 6}</h2>
+    <h2>{console.log("Rana Vikram Sinha")}</h2>
+    {title}
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent3 />);
